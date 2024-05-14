5f021072336740348718fbedea39be80b5e77406
jfurlong <jfurlong@indeed.com>
2024-05-14 15:14:19 +0900

    Adding stuff2

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -8,8 +8,8 @@ function RecipeList({recipes, deleteRecipe}) {
           <td>{recipe.name}</td>
           <td>{recipe.cuisine}</td>
           <td><img src={recipe.photo} alt="Food"/></td>
-          <td><p>{recipe.ingredients}</p></td>
-          <td><p>{recipe.preparation}</p></td>
+          <td className="content_td"><p>{recipe.ingredients}</p></td>
+          <td className="content_td"><p>{recipe.preparation}</p></td>
           <td>
             <button name="delete" onClick={() => deleteRecipe(index)}>Delete</button>
           </td>
